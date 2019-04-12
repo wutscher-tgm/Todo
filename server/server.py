bbe5835effe5df4521d4c1acab1e846d32029809
Richard Wutscher <richard.wutscher@gmail.com>
2019-04-12 10:24:12 +0200

    started server implementation

--- server/server.py
+++ server/server.py
@@ -1,9 +1,45 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
+"""
+Todo-List Routes
+"""
+@app.route("/list/", methods=['GET'])
+def getList(list):
     return "Hello World!"
+
+@app.route("/list/<list>/", methods=['POST'])
+def createList(list):
+    return list
+
+@app.route("/list/<list>/", methods=['PUT'])
+def updateList(list):
+    return list
+
+@app.route("/list/<list>/", methods=['DELETE'])
+def deleteList(list):
+    return list
+
+
+"""
+Todo-Item Routes
+"""
+
+@app.route("/item/<list>/", methods=['GET'])
+def getItem(list):
+    return list
+
+@app.route("/item/<list>/<item>/", methods=['POST'])
+def createItem(list, item):
+    return list+item
+
+@app.route("/item/<list>/<item>/", methods=['PUT'])
+def updateItem(list, item):
+    return list+item
+
+@app.route("/item/<list>/<item>/", methods=['DELETE'])
+def deleteItem(list, item):
+    return list+item
 
 if __name__ == '__main__':
     app.run(debug=True)
