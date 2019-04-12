4c10ca63ee0c4296d77dca9d704d526c06e8edcd
Richard Wutscher <richard.wutscher@gmail.com>
2019-04-12 10:35:44 +0200

    started database implementation

---
+++ server/database.py
@@ -0,0 +1,36 @@
+
+class Database:
+    def __init__():
+        pass
+
+    """
+    Todo-List Functions
+    """
+
+    def getList():
+        pass
+
+    def addList():
+        pass
+    
+    def updateList():
+        pass
+    
+    def deleteList():
+        pass
+    
+    """
+    Todo-Item Functions
+    """
+
+    def getItem():
+        pass
+
+    def addItem():
+        pass
+    
+    def updateItem():
+        pass
+    
+    def deleteItem():
+        pass
